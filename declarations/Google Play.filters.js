d89122eb5db48ce757d556b47c534d48d4efbb7a
Nicolas Dupont <npg.dupont@gmail.com>
2021-11-19 09:43:35 +0100

    Update filter to remove OTA dependency

--- declarations/Google Play.filters.js
+++ declarations/Google Play.filters.js
@@ -1,7 +1,6 @@
 import DataURIParser from 'datauri/parser.js';
 import mime from 'mime';
-
-import fetch from '../src/app/fetcher/htmlOnlyFetcher.js';
+import fetch from 'node-fetch';
 
 const dataURI = new DataURIParser();
 
@@ -14,9 +13,9 @@ export async function downloadImages(document, { fetch: baseUrl, select: selecto
     }
 
     const imageUrl = new URL(src, baseUrl).href; // Ensure url is absolute
-
-    const { mimeType, content } = await fetch(imageUrl);
-
+    const response = await fetch(imageUrl);
+    const mimeType = response.headers.get('content-type');
+    const content = await response.arrayBuffer();
     const extension = mime.getExtension(mimeType);
 
     images[index].src = dataURI.format(extension, content).content;
