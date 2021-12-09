1de76ac8de5ec3d9c6bb5b15ffbee594e4687ba1
Martin Ratinaud <martinratinaud@gmail.com>
2021-12-09 15:51:31 +0400

    Update instagram developer terms so that it does not blink

--- declarations/Instagram.filters.js
+++ declarations/Instagram.filters.js
@@ -1,1 +1,9 @@
 export { cleanUrls } from './Facebook.filters.js';
+
+export const replaceInfoImageWithEmoji = (document) => {
+  const infoImages = document.querySelectorAll('img[src*="/851547_537948159656190_540847388_n.png?"]');
+
+  infoImages.forEach(infoImage => {
+    infoImage.replaceWith('ℹ️')
+  });
+}
