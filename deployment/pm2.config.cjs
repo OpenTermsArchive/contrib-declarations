2f4c8aac957f7af36c6e13dfd90ab5f73476f6ea
Clément Biron <clement.biron@gmail.com>
2024-06-06 10:54:29 +0200

    Update engine and deployment (#1482)
    * Update engine
    
    * Update deployment
    
    * Update deployment workflow

---
+++ deployment/pm2.config.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ota',
+      script: 'npm',
+      args: 'run start:schedule',
+      max_restarts: 2,
+      min_uptime: '1h', // Set a relatively high duration (more than the longest run) so that restarts that occur before this duration has elapsed are considered unstable.
+      restart_delay: 3 * 60 * 60 * 1000,  // likely related to a connectivity problem that will take some time to be fixed
+    },
+    {
+      name: 'ota-api',
+      script: 'npm',
+      args: 'run start:api',
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 1000,
+      exponential_backoff_restart_delay: true
+    },
+    {
+      name: 'ota-release',
+      script: 'npm',
+      args: 'run dataset:schedule',
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 60 * 60 * 1000,  // likely related to a GitHub availability problem that will take some time to be fixed
+      exponential_backoff_restart_delay: true
+    }
+  ],
+};
