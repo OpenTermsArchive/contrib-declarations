f8180c79122bf681f89ade35023c8268015a114f
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-09-16 21:39:24 +0300

    Fix email protection also for security email

--- declarations/GitLab.filters.js
+++ declarations/GitLab.filters.js
@@ -1,5 +1,4 @@
-export function removeCloudflareEmailProtection(document) {
-  const possibleEmail = 'DPO@gitlab.com';
+function removeCloudflareEmailProtection(document, possibleEmail) {
   const placeholder = 'EMAIL_PLACEHOLDER';
   const replaceWith = `(email protected, possibly ${placeholder})`;
 
@@ -13,5 +12,15 @@ export function removeCloudflareEmailProtection(document) {
   // it doesn't. In both cases we want to show the "email protected" text.
   document.body.innerHTML = document.body.innerHTML.replaceAll('[\[email protected\]](https://about.gitlab.com/cdn-cgi/l/email-protection)', replaceWith);
   document.body.innerHTML = document.body.innerHTML.replaceAll(possibleEmail, replaceWith);
-  document.body.innerHTML = document.body.innerHTML.replaceAll(placeholder, possibleEmail);
+  document.body.innerHTML = document.body.innerHTML.replaceAll(placeholder, possibleEmail.toLowerCase());
+
+}
+
+export function removeDpoEmailProtection(document) {
+  removeCloudflareEmailProtection(document, 'DPO@gitlab.com')
+}
+
+export function removeSecurityEmailProtection(document) {
+  removeCloudflareEmailProtection(document, 'security@gitlab.com')
+  removeCloudflareEmailProtection(document, 'Security@gitlab.com')
 }
