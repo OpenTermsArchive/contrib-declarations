9e8c54c3c711d30ae461d18a8db8f8b7e0fecaca
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-09-16 21:33:23 +0300

    Lint filters file

--- declarations/GitLab.filters.js
+++ declarations/GitLab.filters.js
@@ -1,16 +1,17 @@
 export function removeCloudflareEmailProtection(document) {
-  const possibleEmail = "DPO@gitlab.com"
-  const placeholder = "EMAIL_PLACEHOLDER"
-  const replaceWith = `(email protected, possibly ${placeholder})`
-  
+  const possibleEmail = 'DPO@gitlab.com';
+  const placeholder = 'EMAIL_PLACEHOLDER';
+  const replaceWith = `(email protected, possibly ${placeholder})`;
+
   const emails = document.querySelectorAll('.__cf_email__');
+
   emails.forEach(email => {
     email.outerHTML = replaceWith;
   });
 
-  // These make the fetching stable, sometimes it displays the email and sometimes 
+  // These make the fetching stable, sometimes it displays the email and sometimes
   // it doesn't. In both cases we want to show the "email protected" text.
-  document.body.innerHTML = document.body.innerHTML.replaceAll("[\[email protected\]](https://about.gitlab.com/cdn-cgi/l/email-protection)", replaceWith)
-  document.body.innerHTML = document.body.innerHTML.replaceAll(possibleEmail, replaceWith)
-  document.body.innerHTML = document.body.innerHTML.replaceAll(placeholder, possibleEmail)
+  document.body.innerHTML = document.body.innerHTML.replaceAll('[\[email protected\]](https://about.gitlab.com/cdn-cgi/l/email-protection)', replaceWith);
+  document.body.innerHTML = document.body.innerHTML.replaceAll(possibleEmail, replaceWith);
+  document.body.innerHTML = document.body.innerHTML.replaceAll(placeholder, possibleEmail);
 }
