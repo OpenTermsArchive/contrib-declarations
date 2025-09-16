52f0e0280f22be45352a0fe825ecfe1405d7d7bb
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-09-16 21:43:09 +0300

    Fix linting

--- declarations/GitLab.filters.js
+++ declarations/GitLab.filters.js
@@ -10,17 +10,16 @@ function removeCloudflareEmailProtection(document, possibleEmail) {
 
   // These make the fetching stable, sometimes it displays the email and sometimes
   // it doesn't. In both cases we want to show the "email protected" text.
-  document.body.innerHTML = document.body.innerHTML.replaceAll('[\[email protected\]](https://about.gitlab.com/cdn-cgi/l/email-protection)', replaceWith);
+  document.body.innerHTML = document.body.innerHTML.replaceAll('[\\[email protected\\]](https://about.gitlab.com/cdn-cgi/l/email-protection)', replaceWith);
   document.body.innerHTML = document.body.innerHTML.replaceAll(possibleEmail, replaceWith);
   document.body.innerHTML = document.body.innerHTML.replaceAll(placeholder, possibleEmail.toLowerCase());
-
 }
 
 export function removeDpoEmailProtection(document) {
-  removeCloudflareEmailProtection(document, 'DPO@gitlab.com')
+  removeCloudflareEmailProtection(document, 'DPO@gitlab.com');
 }
 
 export function removeSecurityEmailProtection(document) {
-  removeCloudflareEmailProtection(document, 'security@gitlab.com')
-  removeCloudflareEmailProtection(document, 'Security@gitlab.com')
+  removeCloudflareEmailProtection(document, 'security@gitlab.com');
+  removeCloudflareEmailProtection(document, 'Security@gitlab.com');
 }
