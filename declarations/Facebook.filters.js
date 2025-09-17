ea6e0f8d501e9d8ba50f31c48f7f9c796349a7d6
Nicolas Dupont <npg.dupont@gmail.com>
2025-09-17 11:09:36 +0200

    Update engine (#3966)
    * Update engine
    
    * Replace custom filter by builtin filter
    
    * Lint
    
    * Fix declaration
    
    * Cleanup filters files
    
    * Update engine
    
    * Lint
    
    * Update engine

--- declarations/Facebook.filters.js
+++ declarations/Facebook.filters.js
@@ -40,23 +40,3 @@ export function removeEmptyAnchorsLinks(document) {
   Array.from(document.querySelectorAll('[href="#"]')).map(link =>
     link.removeAttribute('href'));
 }
-
-export function removeTrackingIDs(document) {
-  removeQueryParam(document, 'h');
-}
-
-export function removeLocaleFromUrls(document) {
-  removeQueryParam(document, 'locale2');
-}
-
-function removeQueryParam(document, queryParam) {
-  document.querySelectorAll('a').forEach(el => {
-    const href = el.getAttribute('href');
-    const params = new URLSearchParams(href);
-
-    if (params.has(queryParam)) {
-      params.delete(queryParam);
-      el.setAttribute('href', params.toString());
-    }
-  });
-}
--- declarations/Instagram.filters.history.js
+++ declarations/Instagram.filters.history.js
@@ -1,8 +1,34 @@
-import { cleanUrls as facebookCleanUrls } from './Facebook.filters.js';
-
 export const cleanUrls = [
   {
     validUntil: '2022-02-23T13:35:03+01:00',
-    filter: facebookCleanUrls,
+    filter: function cleanUrls(document) {
+      Array.from(document.querySelectorAll('[href="#"]')).map(link => link.removeAttribute('href'));
+      const links = document.querySelectorAll('[href*="https://l.facebook.com/l.php?"],[href*="http://l.facebook.com/l.php?"]');
+
+      links.forEach(link => {
+        link.href = decodeURIComponent(link.href.replace(/&h=\S*/, '').replace(/(\S*)\?u=(\S*)/, '$2'));
+      });
+    },
+  },
+];
+
+export const removeTrackingIDs = [
+  {
+    validUntil: '2025-09-16T12:00:00+01:00',
+    filter: function removeTrackingIDs(document) {
+      document.querySelectorAll('a').forEach(el => {
+        const href = el.getAttribute('href');
+        const params = new URLSearchParams(href);
+
+        if (params.has('h')) {
+          params.set('h', 'removed');
+          el.setAttribute('href', params.toString());
+        }
+        if (params.has('e')) {
+          params.set('e', 'removed');
+          el.setAttribute('href', params.toString());
+        }
+      });
+    },
   },
 ];
