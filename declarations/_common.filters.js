2e9d388cab3c1fe6385d9bcaef4368e6272151ac
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-09-17 12:25:24 +0300

    Fix snapchat and add filter for gclid parameters
    Fix Snapchat

--- declarations/_common.filters.js
+++ declarations/_common.filters.js
@@ -5,3 +5,13 @@ export function removeSIDfromUrls(document) {
     link.href = link.href.replace(/(\?.*sid=)(\w{32})/gim, '$1removed');
   });
 }
+
+export function removeGclidFromUrls(document) {
+  const links = document.querySelectorAll('a');
+
+  links.forEach(link => {
+    const url = new URL(link.href);
+    url.searchParams.delete("gclid")
+    link.href = url.toString()
+  });
+}
