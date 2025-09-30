93fd3618e2e056ba1d16414c8887b49c5d8ccf1a
Eetu Mäenpää <me@eetumaenpaa.fi>
2025-09-30 11:21:07 +0300

    Update to new engine

--- declarations/GitLab.filters.js
+++ declarations/GitLab.filters.js
@@ -1,4 +1,4 @@
-function removeCloudflareEmailProtection(document, possibleEmail) {
+export function removeCloudflareEmailProtection(document, possibleEmail) {
   const placeholder = 'EMAIL_PLACEHOLDER';
   const replaceWith = `(email protected, possibly ${placeholder})`;
 
@@ -13,10 +13,6 @@ function removeCloudflareEmailProtection(document, possibleEmail) {
   document.body.innerHTML = document.body.innerHTML.replaceAll('[\\[email protected\\]](https://about.gitlab.com/cdn-cgi/l/email-protection)', replaceWith);
   document.body.innerHTML = document.body.innerHTML.replaceAll(possibleEmail, replaceWith);
   document.body.innerHTML = document.body.innerHTML.replaceAll(placeholder, possibleEmail.toLowerCase());
-}
-
-export function removeDpoEmailProtection(document) {
-  removeCloudflareEmailProtection(document, 'DPO@gitlab.com');
 }
 
 export function removeSecurityEmailProtection(document) {
